7087c6356194eafd2daa751ab15623f4b1d189e1
solomonbestz <kingsolodawarrior@gmail.com>
2026-02-09 23:19:30 +0100

    feats: Little deno settings and Lox class configurations

--- scanner.ts
+++ scanner.ts
@@ -1,4 +1,16 @@
 
+class Scanner {
+    source: string | null
+
+    constructor(source: string | null){
+        this.source = source
+    }
+
+    scanTokens(){
+        return []
+    }
+}
+
 class Lox {
     hadError: boolean
     
@@ -7,15 +19,29 @@ class Lox {
         console.log("Usage: TLox [script]");
     }
 
-    error(line: number, message: String){
+    error(line: number, message: string){
         this.report(line,"", message)
     }
 
-    report(line: number, where: String, message: String){
+    report(line: number, where: string, message: string){
         console.log(
             "[line " + line + "] Error " + where + ": " + message
         )
         this.hadError = true
+    }
+
+    runFile(){
+        if(this.hadError) Deno.exit(0)
+    }
+
+    runPrompt(){
+        const input = prompt();
+        this.run(input)
+    }
+
+    run(source: string | null){
+        const scanner = new Scanner(source)
+        tokens: [] = scanner.scanTokens(); 
     }
 }
 
