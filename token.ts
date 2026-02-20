bee11d9366e46f81e28e782cff0ec9fe7fd38b4f
solomonbestz <kingsolodawarrior@gmail.com>
2026-02-20 23:02:24 +0100

    feat: Added the token class

---
+++ token.ts
@@ -0,0 +1,19 @@
+import { TokenType } from "./tokenType.ts";
+
+class Token{
+    type: TokenType
+    lexeme: string
+    literal: object
+    line: number
+
+    constructor(type: TokenType, lexeme: string, literal: object, line: number){
+        this.type = type
+        this.lexeme = lexeme
+        this.literal = literal
+        this.line = line
+    }
+
+    toString(){
+        return this.type + " " + this.lexeme + " " + this.literal
+    }
+}
