b4a4d9a9cf929810d1f8749962cd2bb21dbec7f5
solomonbestz <kingsolodawarrior@gmail.com>
2026-02-22 00:25:57 +0100

    fix: Added a type interface for the Literal

--- token.ts
+++ token.ts
@@ -1,12 +1,14 @@
 import { TokenType } from "./tokenType.ts";
 
+export type Literal = string | number | boolean | null;
+
 export class Token{
     type: TokenType
     lexeme: string
-    literal: object
+    literal: Literal
     line: number
 
-    constructor(type: TokenType, lexeme: string, literal: object, line: number){
+    constructor(type: TokenType, lexeme: string, literal: Literal, line: number){
         this.type = type
         this.lexeme = lexeme
         this.literal = literal
